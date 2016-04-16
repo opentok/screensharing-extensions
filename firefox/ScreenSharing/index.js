1199b352985b99e01945c3fb529c02d1c9a39e1e
Jeff Swartz <swartz@tokbox.com>
2016-04-15 17:48:53 -0700

    Fixing code for matching the extension loadReason.

--- firefox/ScreenSharing/index.js
+++ firefox/ScreenSharing/index.js
@@ -5,7 +5,7 @@ var gDomains = ['*.example.com'];
 
 exports.main = function (options) {
 
-  if (options.loadReason === 'install' || 'enable') {
+  if (options.loadReason === 'install' || options.loadReason === 'enable') {
     var curPrefs = prefsService.get(allowDomainsPrefKey).replace(/\s/g, '').split(',');
 
     gDomains.forEach(function(domain){
@@ -19,7 +19,7 @@ exports.main = function (options) {
 };
 
 exports.onUnload = function (reason) {
-  if (reason === 'uninstall' || 'disable') {
+  if (reason === 'uninstall' || options.loadReason === 'disable') {
     gDomains.forEach(function(domain){
       var curPref = prefsService.get(allowDomainsPrefKey);
       var newPref = curPref.split(',').filter((pref) => pref.trim() != domain).join(',');
